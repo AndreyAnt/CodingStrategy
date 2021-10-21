3bfb77b17e692e023b6939db9ef8b04a00aeaed3
Andrey Antropov <antropov@worki.ru>
2021-10-21 14:00:34 +0300

    Added parsing function

--- Sources/CodingStrategy/ColorStrategy.swift
+++ Sources/CodingStrategy/ColorStrategy.swift
@@ -9,13 +9,7 @@ import UIKit
 
 public struct ColorStrategy: CodingStrategy {
     public static func decode(_ value: String) throws -> UIColor {
-        let color: UIColor?
-        switch value {
-        case let string where string.starts(with: "#"):
-            color = .green
-        default:
-            color = nil
-        }
+        let color: UIColor? = colorFrom(hexString: value)
         
         if color != nil {
             return color!
@@ -27,5 +21,25 @@ public struct ColorStrategy: CodingStrategy {
     public static func encode(_ value: UIColor) -> String {
         let components = value.components
         return "#" + String(components.0 * 255) + String(components.1 * 255) + String(components.2 * 255)
+    }
+    
+    private static func colorFrom(hexString: String) -> UIColor? {
+        var cString:String = hexString.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+
+        guard (cString.count) == 6 { return nil }
+
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
 }
